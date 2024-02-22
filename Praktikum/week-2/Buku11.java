6209d0cac0ea0ea7391c1f69146d18bf2bed51d3
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-02-22 21:21:21 +0700

    feat: new 3 methods refactor: reformat

--- Praktikum/week-2/Buku11.java
+++ Praktikum/week-2/Buku11.java
@@ -1,20 +1,21 @@
 public class Buku11 {
     String judul, pengarang;
-    int halaman, stok, harga;
-    
-    public Buku11(){
+    int halaman, stok, harga, terjual;
+
+    public Buku11() {
 
     }
 
-    public Buku11(String jud, String pg, int hal, int stok, int har){
+    public Buku11(String jud, String pg, int hal, int stok, int har) {
         judul = jud;
         pengarang = pg;
         halaman = hal;
         this.stok = stok;
         harga = har;
+        terjual = 0;
     }
 
-    void tampilInformasi(){
+    void tampilInformasi() {
         System.out.println("Judul: " + judul);
         System.out.println("Pengarang: " + pengarang);
         System.out.println("Jumlah halaman: " + halaman);
@@ -22,21 +23,42 @@ public class Buku11 {
         System.out.println("Harga Rp: " + harga);
     }
 
-    void terjual(int jml){
-        if(stok <= 0){
+    void terjual(int jml) {
+        if (stok <= 0) {
             System.out.println("Stock habis!");
-        } else if(stok - jml < 0){
+        } else if (stok - jml < 0) {
             System.out.println("Stock tidak cukup");
-        } else{
+        } else {
             stok -= jml;
+            terjual += jml;
         }
     }
 
-    void restock(int jml){
+    void restock(int jml) {
         stok += jml;
     }
 
-    void gantiHarga(int hrg){
+    void gantiHarga(int hrg) {
         harga = hrg;
+    }
+
+    int hitungHargaTotal() {
+        return harga * terjual;
+    }
+
+    int hitungDiskon() {
+        int x = hitungHargaTotal();
+        if (x > 150000) {
+            x *= 12 / 100;
+        } else if (x >= 75000 && x <= 150000) {
+            x *= 5 / 100;
+        } else {
+            return 0;
+        }
+        return x;
+    }
+
+    int hitungHargaBayar() {
+        return hitungHargaTotal() - hitungDiskon();
     }
 }
