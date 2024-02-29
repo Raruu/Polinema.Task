e8c187cb27924655e6766b206452b2eafccc62ab
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-02-29 14:19:43 +0700

    feat: Soal no 3

--- Praktikum/week-3/Praktikum/MainSoal2.java
+++ Praktikum/week-3/Praktikum/MainSoal2.java
@@ -6,9 +6,11 @@ public class MainSoal2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Mahasiswa[] arrMahasiswa = new Mahasiswa[3];
+        double rataIpk = 0;
+        double ipkTerbesar = 0;
 
         for (int i = 0; i < arrMahasiswa.length; i++) {
-            System.out.println("Masukkan data Mahasiswa ke-" + (i+1));
+            System.out.println("Masukkan data Mahasiswa ke-" + (i + 1));
             System.out.print("Masukkan nama: ");
             String nama = sc.nextLine();
             System.out.print("Masukkan nim: ");
@@ -18,13 +20,20 @@ public class MainSoal2 {
             System.out.print("Masukkan IPK: ");
             double ipk = sc.nextDouble();
             sc.nextLine();
+            rataIpk += ipk;
+            if (ipkTerbesar < ipk)
+                ipkTerbesar = ipk;
             arrMahasiswa[i] = new Mahasiswa(nama, nim, kelamin, ipk);
         }
         System.out.println();
         for (int i = 0; i < arrMahasiswa.length; i++) {
-            System.out.println("Data Mahasiswa ke-" + (i+1));
+            System.out.println("Data Mahasiswa ke-" + (i + 1));
             arrMahasiswa[i].printData();
         }
+
+        rataIpk /= arrMahasiswa.length;
+        System.out.println("\nRata-rata IPK: " + rataIpk);
+        System.out.println("IPK Terbesar: " + ipkTerbesar);
 
         sc.close();
     }
