28eb268dc9f928c2733cc08b04c2ad4a7e73cbdf
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-03-01 09:17:14 +0700

    feat: Praktikum 3 -> method

--- Praktikum/week-3/Praktikum/MainSoal2.java
+++ Praktikum/week-3/Praktikum/MainSoal2.java
@@ -3,11 +3,27 @@ package Praktikum;
 import java.util.Scanner;
 
 public class MainSoal2 {
+    public static void printRataIPK(Mahasiswa[] arr){
+        double rataIpk = 0;
+        for (int i = 0; i < arr.length; i++) {
+            rataIpk += arr[i].ipk;
+        }
+        rataIpk /= arr.length;
+        System.out.println("\nRata-rata IPK: " + rataIpk);
+    }
+
+    public static void printIPKTerbesar(Mahasiswa[] arr){
+        double ipkTerbesar = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (ipkTerbesar < arr[i].ipk)
+                ipkTerbesar = arr[i].ipk;
+        }
+        System.out.println("IPK Terbesar: " + ipkTerbesar);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Mahasiswa[] arrMahasiswa = new Mahasiswa[3];
-        double rataIpk = 0;
-        double ipkTerbesar = 0;
 
         for (int i = 0; i < arrMahasiswa.length; i++) {
             System.out.println("Masukkan data Mahasiswa ke-" + (i + 1));
@@ -19,22 +35,19 @@ public class MainSoal2 {
             char kelamin = sc.nextLine().charAt(0);
             System.out.print("Masukkan IPK: ");
             double ipk = sc.nextDouble();
-            sc.nextLine();
-            rataIpk += ipk;
-            if (ipkTerbesar < ipk)
-                ipkTerbesar = ipk;
+            sc.nextLine();            
             arrMahasiswa[i] = new Mahasiswa(nama, nim, kelamin, ipk);
         }
         System.out.println();
         for (int i = 0; i < arrMahasiswa.length; i++) {
             System.out.println("Data Mahasiswa ke-" + (i + 1));
             arrMahasiswa[i].printData();
-        }
+        }       
 
-        rataIpk /= arrMahasiswa.length;
-        System.out.println("\nRata-rata IPK: " + rataIpk);
-        System.out.println("IPK Terbesar: " + ipkTerbesar);
-
+        System.out.println();
+        printRataIPK(arrMahasiswa);
+        printIPKTerbesar(arrMahasiswa);
+        
         sc.close();
     }
 }
