2115ca140de7ab47e5cd2aa40a182e942bcdc39e
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-04-02 21:25:21 +0700

    init

---
+++ Praktikum/week-7/BukuMain12.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class BukuMain12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        PencarianBuku12 data = new PencarianBuku12();
+        int jumBuku = 5;
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Masukkan Data buku secara urut daru KodeBuku terkecil:");
+        for (int i = 0; i < jumBuku; i++) {
+            System.out.println("-----------------------------");
+            System.out.print("Kode Buku \t : ");
+            int kodeBuku = scanner.nextInt();
+            scanner.nextLine();
+            System.out.print("Judul Buku \t : ");
+            String judulBuku = scanner.nextLine();
+            System.out.print("Tahun Terbit \t : ");
+            int tahunTerbit = scanner.nextInt();
+            scanner.nextLine();
+            System.out.print("Pengarang \t : ");
+            String pengarang = scanner.nextLine();
+            System.out.print("Stock \t\t : ");
+            int stock = scanner.nextInt();
+            scanner.nextLine();
+
+            Buku12 m = new Buku12(kodeBuku, judulBuku, tahunTerbit, pengarang, stock);
+            data.tambah(m);
+        }
+        System.out.println("--------------------------------------------------");
+        System.out.println("Data keseluruhan Buku : ");
+        data.tampil();
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Pencarian Data : ");
+        System.out.println("Masukkan Kode Buku yang ingin dicari : ");
+        System.out.print("Kode Buku : ");
+        int cari = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Menggunakan sequential search");
+        int posisi = data.FindSeqSearch(cari);
+        data.TampilPosisi(cari, posisi);
+        data.TampilData(cari, posisi);
+
+        Buku12 dataBuku12 = data.FindBuku(cari);
+        dataBuku12.tampilDataBuku();
+
+        scanner.close();
+    }
+}
---
+++ Praktikum/week-7/PencarianBuku12.java
@@ -0,0 +1,59 @@
+public class PencarianBuku12 {
+    Buku12[] listBk = new Buku12[5];
+    int idx;
+
+    public void tambah(Buku12 m) {
+        if (idx < listBk.length) {
+            listBk[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!");
+        }
+    }
+
+    public void tampil() {
+        for (Buku12 buku12 : listBk) {
+            buku12.tampilDataBuku();
+        }
+    }
+
+    public int FindSeqSearch(int cari) {
+        int posisi = -1;
+        for (int i = 0; i < listBk.length; i++) {
+            if (listBk[i].kodeBuku == cari) {
+                posisi = i;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    public void TampilPosisi(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("data: " + x + " ditemukan pada index " + pos);
+        } else {
+            System.out.println("data: " + x + " tidak ditemukan");
+        }
+    }
+
+    public void TampilData(int x, int pos){
+        if(pos != -1){
+            System.out.println("Kode Buku\t : " + x);
+            System.out.println("Judul\t\t : " + listBk[pos].judulBuku);
+            System.out.println("Tahun Terbit\t : " + listBk[pos].tahunTerbit);
+            System.out.println("Pengarang\t : " + listBk[pos].pengarang);
+            System.out.println("Stock\t\t : " + listBk[pos].stock);
+        } else {
+            System.out.println("data " + x + " tidak ditemukan");
+        }
+    }
+
+    public Buku12 FindBuku(int kodeBuku){
+        for (int i = 0; i < listBk.length; i++) {
+            if (listBk[i].kodeBuku == kodeBuku) {
+                return listBk[i];
+            }
+        }
+        return null;
+    }
+}
