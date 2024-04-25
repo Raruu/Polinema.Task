54e05ac0022ee85865cc177ef20933b21bb299e0
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-04-25 18:39:12 +0700

    init

---
+++ Praktikum/week-9/Tugas/Pembeli.java
@@ -0,0 +1,15 @@
+package Tugas;
+
+public class Pembeli {
+    String nama;
+    int noHP;
+
+    Pembeli() {
+        
+    }
+    
+    Pembeli(String nama, int noHP) {
+        this.nama = nama;
+        this.noHP = noHP;
+    }
+}
