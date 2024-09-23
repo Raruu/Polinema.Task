59b8f0d3c1d82ca91681db22ad39837f0f81a555
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-09-23 14:26:33 +0700

    fix cancle

--- Semester-3/Desain dan Pemrograman Web/meeting-4/TugasJobsheet4/jawascript.js
+++ Semester-3/Desain dan Pemrograman Web/meeting-4/TugasJobsheet4/jawascript.js
@@ -1,17 +1,21 @@
 function changeHeaderTitle() {
-    document.getElementById("header-title").textContent = prompt("Masukkan judul baru", "Konnchiwa~ Sekai~");
+  var result = prompt("Masukkan judul baru", "Konnchiwa~ Sekai~");
+  if (result != null) {
+    document.getElementById("header-title").textContent = result;
+  }
 }
 
 var minutes = 0;
 var seconds = 0;
 
 function incrementSeconds() {
-    var timeSecs = document.getElementById("time-secs");
-    seconds += 1;
-    if(seconds >= 59){
-        minutes += 1;
-        seconds = 0;
-    }
-    timeSecs.innerText = ((minutes > 0) ? (minutes + " Menit ") : "") +  seconds + " Detik";
-    console.log(seconds);
+  var timeSecs = document.getElementById("time-secs");
+  seconds += 1;
+  if (seconds >= 59) {
+    minutes += 1;
+    seconds = 0;
+  }
+  timeSecs.innerText =
+    (minutes > 0 ? minutes + " Menit " : "") + seconds + " Detik";
+  console.log(seconds);
 }
