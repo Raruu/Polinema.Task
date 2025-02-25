346aa0b0f763102b05354e0dc9f9514f69445217
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-02-25 07:34:33 +0700

    weblnjt/JS3: Praktikum 6

---
+++ Semester-4/WebLanjut/week-3/PWL_POS/app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data);
+
+        $data = ['nama' => 'Pelanggan Pertama'];
+        UserModel::where('username', 'customer-1')->update($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
