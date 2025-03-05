258469b13de4d660c6374c1bb9938e681558b0d2
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-03-05 10:09:25 +0700

    weblnjt/JS4: Praktikum 2.6

--- Semester-4/WebLanjut/PWL_POS/app/Http/Controllers/UserController.php
+++ Semester-4/WebLanjut/PWL_POS/app/Http/Controllers/UserController.php
@@ -32,9 +32,7 @@ class UserController extends Controller
         //     'nama' => 'Manager 3',
         //     'password' => Hash::make('12345')
         // ];
-        // UserModel::create($data);
-
-        // $user = UserModel::all();
+        // UserModel::create($data);        
 
         // JS4: Praktikum 2.1
         // $user = UserModel::find(1);
@@ -96,21 +94,69 @@ class UserController extends Controller
         // $user->isClean(); // true
         // dd($user->isDirty());
 
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ]);
-        $user->username = 'manager12';
+        // $user = UserModel::create([
+        //     'username' => 'manager11',
+        //     'nama' => 'Manager11',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+        // $user->username = 'manager12';
 
-        $user->save();
-        
-        $user->wasChanged(); // true 
-        $user->wasChanged('username'); // true 
-        $user->wasChanged(['username', 'level_id']); // true 
-        $user->wasChanged('nama'); // false 
-        dd($user->wasChanged(['nama', 'username'])); // true
+        // $user->save();
+
+        // $user->wasChanged(); // true 
+        // $user->wasChanged('username'); // true 
+        // $user->wasChanged(['username', 'level_id']); // true 
+        // $user->wasChanged('nama'); // false 
+        // dd($user->wasChanged(['nama', 'username'])); // true
+
+        // JS4: Praktikum 2.6
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
+    }
+
+    public function tambah()
+    {
+        // JS4: Praktikum 2.6
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        // JS4: Praktikum 2.6
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        // JS4: Praktikum 2.6
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        // JS4: Praktikum 2.6
+        $user = UserModel::find($id);
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
+        $user->save();
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        // JS4: Praktikum 2.6
+        $user = UserModel::find($id);
+        $user->delete();
+        return redirect('/user');
     }
 }
--- Semester-4/WebLanjut/PWL_POS/resources/views/user.blade.php
+++ Semester-4/WebLanjut/PWL_POS/resources/views/user.blade.php
@@ -7,32 +7,34 @@
 
 <body>
     <h1>Data User</h1>
+    {{-- JS4: Praktikum 2.6 --}}
+    <a href="/user/tambah">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
+        {{-- <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
-        </tr>
+        </tr> --}}
 
-        <!-- JS3 -->
-        {{--@foreach ($data as $d)
-        <!-- <tr>
-            <td>{{ $d->user_id }}</td>
-            <td>{{ $d->username }}</td>
-            <td>{{ $d->nama }}</td>
-            <td>{{ $d->level_id }}</td>
-        </tr> -->
+        {{-- JS3  --}}
+        {{-- @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+            </tr>
         @endforeach --}}
         {{-- END JS3 --}}
 
-        <!-- JS4: Praktikum 2.1 -->
-        <tr>
+        {{-- JS4: Praktikum 2.1 --}}
+        {{-- <tr>
             <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
-        </tr>
+        <td>{{ $data->username }}</td>
+        <td>{{ $data->nama }}</td>
+        <td>{{ $data->level_id }}</td>
+        </tr> --}}
 
         {{-- JS4: Praktikum 2.3 --}}
         {{-- <tr>
@@ -41,6 +43,26 @@
         <tr>
             <td>{{ $data }}</td>
         </tr> --}}
+
+        {{-- JS4: Praktikum 2.6 --}}
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+            <th>Aksi</th>
+        </tr>
+        @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a
+                        href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
+            </tr>
+        @endforeach
+
     </table>
 </body>
 
