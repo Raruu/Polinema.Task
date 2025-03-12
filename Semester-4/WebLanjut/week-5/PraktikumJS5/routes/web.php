8631b1c69604e0093a18fb48edde09a7694408d1
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-03-12 09:50:54 +0700

    refactor

--- Semester-4/WebLanjut/week-5/PraktikumJS5/routes/web.php
+++ Semester-4/WebLanjut/week-5/PraktikumJS5/routes/web.php
@@ -20,9 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/kategori/create', [KategoriController::class, 'create']); 
 Route::post('/kategori', [KategoriController::class, 'store']);
@@ -31,8 +28,10 @@ Route::post('/kategori/edit', [KategoriController::class, 'update']);
 Route::get('/kategori/hapus/{id}', [KategoriController::class, 'delete']);
 
 // JS4: Praktikum 2.6
-// Route::get('/user/tambah', [UserController::class, 'tambah']);
-// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
