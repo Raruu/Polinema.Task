aad74f6b590f882d312a72630992dbb8d49af7a9
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-03-15 04:13:47 +0700

    weblnjt/JS6: buatlah form untuk tabel m_user dan m_level

--- Semester-4/WebLanjut/week-6/PraktikumJS6/routes/web.php
+++ Semester-4/WebLanjut/week-6/PraktikumJS6/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\POSController;
 use App\Http\Controllers\UserController;
+use App\Models\LevelModel;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,13 +23,31 @@ Route::get('/', function () {
 });
 
 Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/kategori/create', [KategoriController::class, 'create']); 
+Route::get('/kategori/create', [KategoriController::class, 'create']);
 Route::post('/kategori', [KategoriController::class, 'store']);
 Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit']);
 Route::post('/kategori/edit', [KategoriController::class, 'update']);
 Route::get('/kategori/hapus/{id}', [KategoriController::class, 'delete']);
 
 Route::resource('m_user', POSController::class);
+
+Route::group(['prefix' => 'form'], function () {
+    Route::get('/m_user', function () {
+        $page = (object) [
+            'title' => 'Daftar user yang terdaftar dalam sistem'
+        ];
+        $activeMenu = 'user'; // set menu yang sedang aktif 
+        $level = LevelModel::all();
+        return view('form.m_user', ['page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
+    });
+    Route::get('/m_level', function () {
+        $page = (object)[
+            'title' => 'Daftar level yang terdaftar dalam sistem'
+        ];
+        $activeMenu = 'level';
+        return view('form.m_level', ['page' => $page, 'activeMenu' => $activeMenu]);
+    });
+});
 
 // JS4: Praktikum 2.6
 Route::get('/level', [LevelController::class, 'index']);
