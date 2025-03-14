6ed977519b12a38c80f1ab31156319e9f48c3d5a
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-03-14 11:13:18 +0700

    weblnjt/JS7: Praktikum 4 – Implementasi Filtering Datatables

--- Semester-4/WebLanjut/week-7/PraktikumJS7/app/Http/Controllers/UserController.php
+++ Semester-4/WebLanjut/week-7/PraktikumJS7/app/Http/Controllers/UserController.php
@@ -20,7 +20,8 @@ class UserController extends Controller
             'title' => 'Daftar user yang terdaftar dalam sistem'
         ];
         $activeMenu = 'user'; // set menu yang sedang aktif 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all();
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables
@@ -28,6 +29,12 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        // Filter data user berdasarkan level_id 
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+
         return DataTables::of($users)
             ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
