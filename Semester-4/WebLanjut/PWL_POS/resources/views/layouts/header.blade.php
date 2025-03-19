ab89144884371c4e7596378622161b277ee36568
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-03-19 22:07:51 +0700

    PWL_POS: update header

--- Semester-4/WebLanjut/PWL_POS/resources/views/layouts/header.blade.php
+++ Semester-4/WebLanjut/PWL_POS/resources/views/layouts/header.blade.php
@@ -5,19 +5,19 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="../../index3.html" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Contact</a>
+            <a href="https://github.com/Raruu/Polinema.Task/tree/main/Semester-4/WebLanjut/PWL_POS"
+                class="nav-link" target="_blank">
+                <i class="fab fa-github"></i>  PWL_POS
+            </a>
         </li>
     </ul>
 
     <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">             
+    <ul class="navbar-nav ml-auto">
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
             </a>
-        </li>        
+        </li>
     </ul>
 </nav>
