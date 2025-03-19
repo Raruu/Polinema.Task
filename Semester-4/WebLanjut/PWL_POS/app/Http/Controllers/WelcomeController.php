02ad1936da0afa02a03ad801e4fdd6af419cbfc5
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-03-19 20:15:59 +0700

    PWL_POS: feat Dashboard transaksi

--- Semester-4/WebLanjut/PWL_POS/app/Http/Controllers/WelcomeController.php
+++ Semester-4/WebLanjut/PWL_POS/app/Http/Controllers/WelcomeController.php
@@ -2,17 +2,100 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\PenjualanDetailModel;
+use App\Models\PenjualanModel;
+use Carbon\Carbon;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
         $breadcrumb = (object) [
-            'title' => 'Selamat Datang',
-            'list' => ['Home', 'Welcome']
+            'title' => 'Dashboard',
+            'list' => ['Home', 'Dashboard']
         ];
         $activeMenu = 'dashboard';
-        return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+
+        $latestDate = PenjualanModel::max('penjualan_tanggal');
+        $penjualanTrend = PenjualanModel::select('penjualan_id', 'penjualan_tanggal')
+            ->where('penjualan_tanggal', '>=', Carbon::parse($latestDate)->subDays(7))
+            ->orderBy('penjualan_tanggal', 'asc')
+            ->get()
+            ->groupBy(function ($item) {
+                return Carbon::parse($item->penjualan_tanggal)->format('Y-m-d');
+            });
+
+        $penjualanIds = [];
+        foreach ($penjualanTrend as $key => &$value) {
+            $tempData = [];
+            foreach ($value as $key2 => $value2) {
+                if (($jumlah = PenjualanDetailModel::where('penjualan_id', $value2->penjualan_id)
+                    ->sum('jumlah')) > 0) {
+                    $tempData[$value2->penjualan_id] = $jumlah;
+                    $penjualanIds[] = $value2->penjualan_id;
+                }
+                unset($value[$key2]);
+            }
+            $penjualanTrend[$key]['jumlah'] = 0;
+            foreach ($tempData as $value2) {
+                $penjualanTrend[$key]['jumlah'] += $value2;
+            }
+        }
+        unset($value);
+        
+        foreach ($penjualanIds as &$value) {
+            $temp = PenjualanDetailModel::with('barang')
+                ->where('penjualan_id', $value)
+                ->orderByDesc('jumlah')
+                ->get()
+                ->map(function ($item) {
+                    return (object) [
+                        'barang_id' => $item->barang_id,
+                        'barang_nama' => $item->barang->barang_nama,
+                        'harga_beli' => $item->barang->harga_beli,
+                        'harga_jual' => $item->barang->harga_jual,
+                        'jumlah' => $item->jumlah
+                    ];
+                })->toArray();
+
+            $temp = collect($temp)->sortByDesc('jumlah')->first();
+            $penjualanRanking[] = (object) [
+                'penjualan_id' => $value,
+                'barang_id' => $temp->barang_id,
+                'barang_nama' => $temp->barang_nama,
+                'harga_beli' => $temp->harga_beli,
+                'harga_jual' => $temp->harga_jual,
+                'jumlah' => $temp->jumlah
+            ];
+        }
+
+        $penjualanRanking = array_reduce($penjualanRanking, function ($carry, $item) {
+            if (!array_reduce($carry, function ($innerCarry, $innerItem) use ($item) {
+                return $innerCarry || $innerItem->barang_id === $item->barang_id;
+            }, false)) {
+                $carry[] = $item;
+            } else {
+                foreach ($carry as &$value) {
+                    if ($value->barang_id === $item->barang_id && $item->jumlah > $value->jumlah) {
+                        $value = $item;
+                    }
+                }
+                unset($value);
+            }
+            return $carry;
+        }, []);
+
+        usort($penjualanRanking, function ($a, $b) {
+            return $b->jumlah <=> $a->jumlah;
+        });
+        $penjualanRanking = array_slice($penjualanRanking, 0, 5);
+
+        return view('welcome', [
+            'breadcrumb' => $breadcrumb,
+            'activeMenu' => $activeMenu,
+            'penjualanTrend' => $penjualanTrend,
+            'penjualanRanking' => $penjualanRanking,
+        ]);
     }
 }
+
--- Semester-4/WebLanjut/PWL_POS/resources/views/welcome.blade.php
+++ Semester-4/WebLanjut/PWL_POS/resources/views/welcome.blade.php
@@ -1,12 +1,137 @@
 @extends('layouts.template')
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Halo, apa kabar!!!</h3>
-            <div class="card-tools"></div>
-        </div>
-        <div class="card-body">
-            Selamat datang semua, ini adalah halaman utama dari aplikasi ini.
+    <div>
+        <div class="d-flex flex-row" style="gap: 10px; max-width: 99%">
+            <div class="card col-md-6">
+                <div class="card-header border-0">
+                    <div class="d-flex justify-content-between">
+                        <h3 class="card-title">Penjualan 7 hari terakhir</h3>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="d-flex">
+                        <p class="d-flex flex-column">
+                            <span class="text-bold text-lg">
+                                {{ array_sum($penjualanTrend->map(fn($item) => $item['jumlah'])->toArray()) }}
+                            </span>
+                            <span>Produk terjual</span>
+                        </p>
+                    </div>
+
+                    <div class="position-relative mb-4">
+                        <div class="chartjs-size-monitor">
+                            <div class="chartjs-size-monitor-expand">
+                                <div class=""></div>
+                            </div>
+                            <div class="chartjs-size-monitor-shrink">
+                                <div class=""></div>
+                            </div>
+                        </div>
+                        <canvas id="visitors-chart" height="200" width="530"
+                            style="display: block; width: 530px; height: 200px;" class="chartjs-render-monitor"></canvas>
+                    </div>
+                </div>
+            </div>
+            <!-- /.card -->
+
+            <div class="card col-md-6">
+                <div class="card-header border-0">
+                    <h3 class="card-title">TOP 5 Produk terbeli paling banyak</h3>
+                </div>
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-striped table-valign-middle">
+                        <thead>
+                            <tr>
+                                <th>Nama Barang</th>
+                                <th>Harga Beli</th>
+                                <th>Harga Jual</th>
+                                <th>Jumlah</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($penjualanRanking as $item)
+                                <tr>
+                                    <td>{{ $item->barang_nama }}</td>
+                                    <td>Rp. {{ number_format($item->harga_beli, 0, ',', '.') }}</td>
+                                    <td>Rp. {{ number_format($item->harga_jual, 0, ',', '.') }}</td>
+                                    <td>{{ $item->jumlah }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- /.card -->
         </div>
     </div>
 @endsection
+
+@push('js')
+    <script src="{{ asset('adminlte/plugins/chart.js/Chart.min.js') }}"></script>
+    <script>
+        $(function() {
+            'use strict'
+            const ticksStyle = {
+                fontColor: '#495057',
+                fontStyle: 'bold'
+            }
+            const mode = 'index';
+            const intersect = false;
+            const $visitorsChart = $('#visitors-chart')
+            // eslint-disable-next-line no-unused-vars
+
+            const penjualanTrend = <?php echo json_encode($penjualanTrend); ?>;
+            const penjualanTrendValues = Object.values(penjualanTrend).map(item => item.jumlah)
+            var visitorsChart = new Chart($visitorsChart, {
+                data: {
+                    labels: Object.keys(penjualanTrend),
+                    datasets: [{
+                        type: 'line',
+                        data: penjualanTrendValues,
+                        backgroundColor: 'transparent',
+                        borderColor: '#007bff',
+                        pointBorderColor: '#007bff',
+                        pointBackgroundColor: '#007bff',
+                        fill: false
+                    }, ]
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    tooltips: {
+                        mode: mode,
+                        intersect: intersect
+                    },
+                    hover: {
+                        mode: mode,
+                        intersect: intersect
+                    },
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        yAxes: [{
+                            // display: false,
+                            gridLines: {
+                                display: true,
+                                lineWidth: '4px',
+                                color: 'rgba(0, 0, 0, .2)',
+                                zeroLineColor: 'transparent'
+                            },
+                            ticks: $.extend({
+                                beginAtZero: true,
+                                suggestedMax: Math.max(...penjualanTrendValues),
+                            }, ticksStyle)
+                        }],
+                        xAxes: [{
+                            display: true,
+                            gridLines: {
+                                display: false
+                            },
+                            ticks: ticksStyle
+                        }]
+                    }
+                }
+            })
+        })
+    </script>
+@endpush
