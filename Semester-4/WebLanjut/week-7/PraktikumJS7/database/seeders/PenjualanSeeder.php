66fbd7d3f0e24736cfea720747040442e547be91
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-03-25 09:42:55 +0700

    weblnjt/JS-7: init base from JS6-NEW + sedikit PWL_POS

---
+++ Semester-4/WebLanjut/week-7/PraktikumJS7/database/seeders/PenjualanSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PenjualanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = DB::table('m_user')->get();
+        $tanggal = now();
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = $users[rand(0, $users->count() - 1)];
+            DB::table('t_penjualan')->insert([
+                'user_id' => $user->user_id,
+                'pembeli' => substr($user->nama, 0, 50),
+                'penjualan_kode' => 'penjualan' . $i,
+                'penjualan_tanggal' => $tanggal = $tanggal->subDay(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
