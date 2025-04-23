59c64013e4b8ba3f7469346e929b53a0cbafbcad
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-04-23 08:55:41 +0700

    weblnjt/JS11: Praktikum 1 – Implementasi Eloquent Accessor

--- Semester-4/WebLanjut/week-9/PraktikumJS11/app/Models/UserModel.php
+++ Semester-4/WebLanjut/week-9/PraktikumJS11/app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -19,12 +20,20 @@ class UserModel extends Authenticatable implements JWTSubject
     {
         return [];
     }
-    
+
     use HasFactory;
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updated_at'];
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+        'image',
+        'created_at',
+        'updated_at'
+    ];
 
     protected $hidden = ['password']; // jangan di tampilkan saat select 
     protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
@@ -57,5 +66,12 @@ class UserModel extends Authenticatable implements JWTSubject
     public function getRole()
     {
         return $this->level->level_kode;
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
     }
 }
