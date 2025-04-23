d02cf13dc1a5821e229e4b75d8857abfaa0f18e4
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-04-23 10:33:53 +0700

    weblnjt/JS11: Implementasikan API untuk upload file/gambar pada m_barang

--- Semester-4/WebLanjut/week-9/PraktikumJS11/app/Http/Controllers/Api/BarangController.php
+++ Semester-4/WebLanjut/week-9/PraktikumJS11/app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -15,13 +16,44 @@ class BarangController extends Controller
 
     public function store(Request $request)
     {
-        $barang = BarangModel::create($request->all());
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'kategori_id' => ['required', 'integer', 'exists:m_kategori,kategori_id'],
+            'barang_kode' => [
+                'required',
+                'min:3',
+                'max:20',
+                'unique:m_barang,barang_kode'
+            ],
+            'barang_nama' => ['required', 'string', 'max:100'],
+            'harga_beli' => ['required', 'numeric'],
+            'harga_jual' => ['required', 'numeric'],
+            'image' => [
+                'required',
+                'image',
+                'mimes:jpeg,png,jpg,gif,svg,webp',
+                'max:2048'
+            ],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $id = BarangModel::all()->max('barang_id') + 1;
+        $image = $request->file('image');
+        $imageName = '[' . $data['barang_kode'] . ']-' . $data['barang_nama'] . '.webp';
+        $image->storeAs('public/barang_images', $imageName);
+        unset($data['image']);
+        $data['image'] = $imageName;
+
+        $barang = BarangModel::create($data);
         return response()->json($barang, 201);
     }
 
     public function show(BarangModel $barang)
     {
-        return BarangModel::find($barang);
+        return $barang;
     }
 
     public function update(Request $request, BarangModel $barang)
